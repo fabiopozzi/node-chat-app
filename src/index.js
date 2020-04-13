dab07c0f58e8671b6beaefcd93b38f9bc2a9dd65
Fabio Pozzi <fabio@fabiopozzi.net>
2020-04-13 13:46:37 +0200

    Basic node app. Serve only static files in the public/ root folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// sources will be here
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Define path for Express configuration
+const publicDirPath = path.join(__dirname, '../public')
+
+// setup static directory to use
+app.use(express.static(publicDirPath))
+
+app.listen(port, () => {
+    console.log('Server is up on port ', port)
+})
