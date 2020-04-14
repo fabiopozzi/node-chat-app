1b11e639b1a711d658d516770bdc5253be9fd4ce
Fabio Pozzi <fabio@fabiopozzi.net>
2020-04-14 09:22:29 +0200

    First experiments with geolocation API

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,3 +10,13 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     const msg = e.target.elements.message.value
     socket.emit('sendMessage', msg);
 })
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+    })
+})
