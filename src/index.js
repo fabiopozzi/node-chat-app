471cdbed724d438321c8a456cc6132313280a99d
Fabio Pozzi <fabio@fabiopozzi.net>
2020-04-14 07:22:48 +0200

    Add socket.io. Setup basic support on server and client side

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
-const port = process.env.PORT || 3000
+const server = http.createServer(app)
+const io = socketio(server)
 
+const port = process.env.PORT || 3000
 // Define path for Express configuration
 const publicDirPath = path.join(__dirname, '../public')
 
 // setup static directory to use
 app.use(express.static(publicDirPath))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New websocket connection')
+})
+
+server.listen(port, () => {
     console.log('Server is up on port ', port)
 })
