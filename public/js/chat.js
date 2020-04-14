9c845c82dd6fd0f8fc2321c54048afae37f1ce2e
Fabio Pozzi <pozzi.fabio@gmail.com>
2020-04-14 16:24:55 +0200

    enable/disable buttons depending on request state.
    Disable buttons after request has been sent.
    Re-enable them just after the callback confirmation has been received.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,29 @@
 const socket = io();
 
+// Elements selected
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+// sendLocation button
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (msg) => {
     console.log(msg);
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    // disable
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     const msg = e.target.elements.message.value
     socket.emit('sendMessage', msg, (error) => {
+        //re enable and clear
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -16,16 +31,18 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     });
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser');
     }
+    $sendLocationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $sendLocationButton.removeAttribute('disabled')
             console.log('Location shared!')
         })
     })
