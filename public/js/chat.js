f6877c30792f5508b9c4e8d4d61db7fc1ca97426
Fabio Pozzi <fabio@fabiopozzi.net>
2020-04-14 08:59:39 +0200

    Improve code to select form elements in a better way

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,9 +4,9 @@ socket.on('message', (msg) => {
     console.log(msg);
 })
 
-document.querySelector('form').addEventListener('submit', (e) => {
+document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const msg = document.querySelector('input').value;
+    const msg = e.target.elements.message.value
     socket.emit('sendMessage', msg);
 })
